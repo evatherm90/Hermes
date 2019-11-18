d3308fe9e21e584211f7d4c3ff575c912199b871
evatherm90 <eva_therm90@hotmail.com>
2019-11-18 23:56:58 +0200

    Revert "Room-controller-service-repository-jsp"
    This reverts commit 7e12599be8b714f3bd1f7d2909f22ec1c61ec031.
    
    # Conflicts:
    #	src/main/java/com/example/groupProject/sercices/RoomService.java
    #	src/main/java/com/example/groupProject/sercices/RoomServiceImplementation.java
    #	src/main/java/com/example/groupProject/sercices/UserService.java
    #	src/main/resources/application.properties

--- src/main/java/com/example/groupProject/repositories/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.groupProject.repositories;
-
-import com.example.groupProject.model.Room;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoomRepository extends CrudRepository<Room, Integer> {
-}
--- src/main/java/com/example/groupProject/sercices/RoomService.java
+++
@@ -1,5 +0,0 @@
-package com.example.groupProject.sercices;
-
-public interface RoomService {
-    
-}
